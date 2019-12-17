2c29648732ca528e6e330aad575ccb90c5e1decf
Elda Oktaviani <eldaoktaviani5@gmail.com>
2019-12-17 21:54:53 +0700

    able to register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./Layouts/Login/Login";
+import Register from "./Layouts/Register/Register";
 
 class App extends React.Component{
   render() {
     return(
         <Router>
-            <Login/>
+            <Register/>
         </Router>
     )
   }
